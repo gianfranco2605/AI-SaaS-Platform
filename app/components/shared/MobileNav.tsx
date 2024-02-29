bbbc5ce9e8d24faa741c6054435d2a5ab3fa3b95
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-02-29 19:57:03 +0100

    mongo confg, model

--- app/components/shared/MobileNav.tsx
+++ app/components/shared/MobileNav.tsx
@@ -1,16 +1,14 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+"use client";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2">
@@ -23,8 +21,61 @@ const MobileNav = () => {
       </Link>
       <nav className="flex gap-2">
         <SignedIn>
-          <UserButton />
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                height={32}
+                width={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="logo"
+                  width={152}
+                  height={23}
+                />
+                <ul className="-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathname;
+                    return (
+                      <li
+                        key={link.route}
+                        className={`${
+                          isActive && "gradient-text"
+                        } p-18 flex whitespace-nowrap text-dark-700`}
+                      >
+                        <Link
+                          href={link.route}
+                          className="sidebar-link cursor-pointer"
+                        >
+                          <Image
+                            src={link.icon}
+                            alt="logo"
+                            width={24}
+                            height={24}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
         </SignedIn>
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in" />
+            Login
+          </Button>
+        </SignedOut>
       </nav>
     </header>
   );
